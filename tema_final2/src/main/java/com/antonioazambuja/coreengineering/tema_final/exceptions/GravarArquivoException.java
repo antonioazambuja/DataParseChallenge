031a622a173f45514dc45fb47c7ad21a4d30df14
antonioazambuja <antonioazambuja_@hotmail.com>
2019-07-28 11:58:37 -0300

    tema final

---
+++ tema_final2/src/main/java/com/antonioazambuja/coreengineering/tema_final/exceptions/GravarArquivoException.java
@@ -0,0 +1,11 @@
+package com.antonioazambuja.coreengineering.tema_final.exceptions;
+
+public class GravarArquivoException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public GravarArquivoException(String message) {
+		super(message);
+	}
+
+}
---
+++ tema_final2/src/main/java/com/antonioazambuja/coreengineering/tema_final/exceptions/MonitoracaoDiretorioEntradaException.java
@@ -0,0 +1,11 @@
+package com.antonioazambuja.coreengineering.tema_final.exceptions;
+
+public class MonitoracaoDiretorioEntradaException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public MonitoracaoDiretorioEntradaException(String message) {
+		super(message);
+	}
+
+}
