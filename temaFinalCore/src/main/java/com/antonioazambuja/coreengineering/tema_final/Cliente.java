d9fa1c70eaecf1d08cf6c55174c569fb2803adfc
Antonio Azambuja <antonioazambuja_@hotmail.com>
2019-07-29 14:11:44 -0300

    adicionando temaFinalCore

---
+++ temaFinalCore/src/main/java/com/antonioazambuja/coreengineering/tema_final/Cliente.java
@@ -0,0 +1,28 @@
+package com.antonioazambuja.coreengineering.tema_final;
+
+public class Cliente {
+	
+	private Long cnpj;
+	private String nome;
+	private String areaNegocio;
+	
+	public Cliente(Long cnpj, String nome, String areaNegocio) {
+		this.cnpj = cnpj;
+		this.nome = nome;
+		this.areaNegocio = areaNegocio;
+	}
+	
+	public Cliente() { }
+	
+	public Long getCnpj() { return cnpj; }
+	
+	public void setCnpj(Long cnpj) { this.cnpj = cnpj; }
+	
+	public String getNome() { return nome; }
+	
+	public void setNome(String nome) { this.nome = nome; }
+	
+	public String getAreaNegocio() { return areaNegocio; }
+	
+	public void setAreaNegocio(String areaNegocio) { this.areaNegocio = areaNegocio; }
+}
